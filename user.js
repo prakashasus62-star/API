42f96a2df2801cedf9855b591072982006695d53
prakash-stack <prakashasus62@gmail.com>
2026-07-27 23:32:12 +0530

    Add user data module with sample users

---
+++ user.js
@@ -0,0 +1,7 @@
+module.exports = [
+    { id: 1, name: "Prakash", age: 22, city: "Salem" },
+    { id: 2, name: "Rahul", age: 25, city: "Chennai" },
+    { id: 3, name: "Kavin", age: 21, city: "Coimbatore" },
+    { id: 4, name: "Arun", age: 24, city: "Madurai" },
+    { id: 5, name: "John", age: 28, city: "Chennai" }
+];
